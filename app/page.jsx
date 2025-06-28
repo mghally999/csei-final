e1550a679e10ad230921cf78c2d3aaf44bedf29f
Mohammed Ghally <mohammedghallydev@gmail.com>
2025-06-28 14:30:13 +0400

    hiding instructors

--- app/page.jsx
+++ app/page.jsx
@@ -35,7 +35,7 @@ export default function HomePage() {
         <Courses />
         <TestimonialsOne />
         <FeaturesOne />
-        <Instructors />
+        {/* <Instructors /> */}
         <GetApp />
         {/* <Blog /> */}
         <Brands />
