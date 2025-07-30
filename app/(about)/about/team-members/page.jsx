b9011af2ff51cfc008f78fd9f362876860653735
Mohammed Ghally <mohammedghallydev@gmail.com>
2025-07-30 17:36:50 +0400

    3frs

--- app/(about)/about/team-members/page.jsx
+++ app/(about)/about/team-members/page.jsx
@@ -409,7 +409,7 @@ const Administration = () => {
         /* Staff Grid */
         .staff-grid {
           display: grid;
-          grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+          grid-template-columns: 1fr 1fr 1fr;
           gap: 30px;
           margin-bottom: 60px;
         }
