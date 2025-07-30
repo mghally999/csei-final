47e1a259805eaa2833a7404b068a4386072b53e4
Mohammed Ghally <mohammedghallydev@gmail.com>
2025-07-30 13:13:34 +0400

    changing office location

--- data/officeLocation.js
+++ data/officeLocation.js
@@ -3,25 +3,29 @@ export const locationData = [
   {
     location: "Registration Office",
     email: "info@cseiacademy.ae",
-    address: "Al Manama St, Academic City, Dubai",
+    address:
+      "Al Manama St, Dubai Academic City - DOC City Building 4, Campus No: 205,104,105",
     phoneNumber: "04 552 2469",
   },
   {
     location: "Admission Office",
     email: "admission@cseiacademy.ae",
-    address: "Al Manama St, Academic City, Dubai",
+    address:
+      "Al Manama St, Dubai Academic City - DOC City Building 4, Campus No: 205,104,105",
     phoneNumber: "04 552 2469",
   },
   {
     location: "Financial Aid Office",
     email: "accounts@cseiacademy.ae",
-    address: "Al Manama St, Academic City, Dubai",
+    address:
+      "Al Manama St, Dubai Academic City - DOC City Building 4, Campus No: 205,104,105",
     phoneNumber: "04 552 2469",
   },
   {
     location: "International Students Office",
     email: "internationaladmission@cseiacademy.ae",
-    address: "Al Manama St, Academic City, Dubai",
+    address:
+      "Al Manama St, Dubai Academic City - DOC City Building 4, Campus No: 205,104,105",
     phoneNumber: "04 552 2469",
   },
 ];
